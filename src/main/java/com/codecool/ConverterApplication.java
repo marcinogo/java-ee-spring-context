8b736c915c69c6c7e7c277ddef28502257680975
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 13:48:13 +0100

    Add logic for validation and print in case of 0 args in main() method

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -3,5 +3,10 @@ package com.codecool;
 public class ConverterApplication {
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("No input file defined");
+        } else {
+
+        }
     }
 }

642642df0a372122c2b2410afdc4513a9acaeb13
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 15:19:23 +0100

    Create SimpleCsvConverter class with method convert

---
+++ src/main/java/com/codecool/service/SimpleCsvConverter.java
@@ -0,0 +1,11 @@
+package com.codecool.service;
+
+import com.codecool.model.OutputFormat;
+
+import java.io.File;
+
+public class SimpleCsvConverter {
+    public void convert (File file, OutputFormat outputFormat) {
+        System.out.println(String.format("I convert CSV to %p", outputFormat));
+    }
+}

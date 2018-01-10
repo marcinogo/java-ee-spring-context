949b6a32c824fcb29c8182ddf6b25bb18fdb57ec
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 18:42:21 +0100

    Create OutputFormatterFactory class with createByFormat() method

---
+++ src/main/java/com/codecool/factory/OutputFormatterFactory.java
@@ -0,0 +1,22 @@
+package com.codecool.factory;
+
+import com.codecool.model.OutputFormat;
+import com.codecool.view.JsonOutputFormatter;
+import com.codecool.view.OutputFormatter;
+import com.codecool.view.TableOutputFormatter;
+import com.codecool.view.XmlOutputFormatter;
+
+public class OutputFormatterFactory {
+    public OutputFormatter createByFormat(OutputFormat outputFormat) {
+        switch (outputFormat) {
+            case JSON:
+                return new JsonOutputFormatter();
+            case XML:
+                return new XmlOutputFormatter();
+            case TABLE:
+                return new TableOutputFormatter();
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+}

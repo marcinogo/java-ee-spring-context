fec22655b036ea20a53c3fe899f24b344978b476
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 16:52:48 +0100

    Create FileReader class with readData() method

---
+++ src/main/java/com/codecool/dao/FileReader.java
@@ -0,0 +1,16 @@
+package com.codecool.dao;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    public List<List<String>> readData(Path file) {
+        List<String> csvRow = new ArrayList<>();
+        List<List<String>> csvData = new ArrayList<>();
+
+        System.out.println("Just print to console some text, for now ;)");
+
+        return csvData;
+    }
+}

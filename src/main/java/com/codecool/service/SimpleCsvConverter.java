0f867c2892bea131a4eae4da82f0839c614a0b80
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 19:05:19 +0100

    Add field outputFormatterFactory, update constructor, update logic of convert() method to use proper OutputFormatter

--- src/main/java/com/codecool/service/SimpleCsvConverter.java
+++ src/main/java/com/codecool/service/SimpleCsvConverter.java
@@ -1,25 +1,32 @@
 package com.codecool.service;
 
 import com.codecool.dao.FileReader;
+import com.codecool.factory.OutputFormatterFactory;
 import com.codecool.model.OutputFormat;
+import com.codecool.view.OutputFormatter;
 
 import java.nio.file.Path;
 
 public class SimpleCsvConverter {
     private FileReader fileReader;
+    private OutputFormatterFactory outputFormatterFactory;
 
     public SimpleCsvConverter() {
         this.fileReader = new FileReader();
+        this.outputFormatterFactory = new OutputFormatterFactory();
     }
 
     public void convert (Path file) {
-        this.fileReader.readData(file);
         OutputFormat outputFormat = OutputFormat.TABLE;
+
+        OutputFormatter outputFormatter = this.outputFormatterFactory.createByFormat(outputFormat);
         System.out.println(String.format("I convert CSV to %s", outputFormat));
+        outputFormatter.printToConsole(this.fileReader.readData(file));
     }
 
     public void convert (Path file, OutputFormat outputFormat) {
-        this.fileReader.readData(file);
+        OutputFormatter outputFormatter = this.outputFormatterFactory.createByFormat(outputFormat);
         System.out.println(String.format("I convert CSV to %s", outputFormat));
+        outputFormatter.printToConsole(this.fileReader.readData(file));
     }
 }

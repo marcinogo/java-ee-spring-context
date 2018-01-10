3cf6e9f615f00838865fcd6aa92e43ad4bb14f98
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 15:05:23 +0100

    Implement logic to handle given args about input file and output formats in main() method

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -1,12 +1,31 @@
 package com.codecool;
 
+import com.codecool.model.OutputFormat;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ConverterApplication {
     public static void main(String[] args) {
-
         if (args.length == 0) {
             System.out.println("No input file defined");
         } else {
+            final Integer PATH_ARG = args.length - 1;
+            Path CSVPath = Paths.get(args[PATH_ARG]);
 
+            OutputFormat outputFormat;
+
+            if (args.length == 1) {
+                outputFormat = OutputFormat.TABLE;
+            } else if (args.length > 1) {
+                final Integer FORMAT_ARG = 0;
+
+                if (args[FORMAT_ARG].equals("json")) {
+                    outputFormat = OutputFormat.JSON;
+                } else if (args[FORMAT_ARG].equals("xml")) {
+                    outputFormat = OutputFormat.XML;
+                }
+            }
         }
     }
 }

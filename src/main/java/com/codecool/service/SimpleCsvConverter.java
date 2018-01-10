eb9c894b7fa13c3aabf2472fdcf94b7552ab0874
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-10 18:09:48 +0100

    Add field of FileReader type, implement default constructor, change file parameter type in convert() methods

--- src/main/java/com/codecool/service/SimpleCsvConverter.java
+++ src/main/java/com/codecool/service/SimpleCsvConverter.java
@@ -1,16 +1,25 @@
 package com.codecool.service;
 
+import com.codecool.dao.FileReader;
 import com.codecool.model.OutputFormat;
 
-import java.io.File;
+import java.nio.file.Path;
 
 public class SimpleCsvConverter {
-    public void convert (File file) {
+    private FileReader fileReader;
+
+    public SimpleCsvConverter() {
+        this.fileReader = new FileReader();
+    }
+
+    public void convert (Path file) {
+        this.fileReader.readData(file);
         OutputFormat outputFormat = OutputFormat.TABLE;
         System.out.println(String.format("I convert CSV to %s", outputFormat));
     }
 
-    public void convert (File file, OutputFormat outputFormat) {
+    public void convert (Path file, OutputFormat outputFormat) {
+        this.fileReader.readData(file);
         System.out.println(String.format("I convert CSV to %s", outputFormat));
     }
 }

85e49805c137df35be04f69a576f94fd1a9595f5
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-01-12 14:50:17 +0100

    Implement logic to convert data from CSV file to JSON ad display it

--- src/main/java/com/codecool/view/JsonOutputFormatter.java
+++ src/main/java/com/codecool/view/JsonOutputFormatter.java
@@ -1,11 +1,22 @@
 package com.codecool.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.List;
 import java.util.Map;
 
 public class JsonOutputFormatter implements OutputFormatter {
     @Override
     public void printToConsole(List<Map<String, String>> data) {
-        System.out.println("Some random CSV converted to JSON");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String jsonResult = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(data);
+            System.out.println(jsonResult);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 }
